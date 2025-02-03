3ccbe81caed6a830987d0fb08207366901f2ddf5
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2025-02-02 18:09:16 -0600

    tmxToTerrain now uses updated xml return type

--- lib/tmx-to-terrain.ts
+++ lib/tmx-to-terrain.ts
@@ -1,32 +1,30 @@
 import parseTmxXml from "@/lib/parseTmxXml.ts";
-import tilesetIdToTerrainId from "../lookup-tables/tileset-id-to-terrain-id.ts";
+import tilesetIdToTerrainId from "@/lookup-tables/tileset-id-to-terrain-id.ts";
 import terrainIdToName from "@/lookup-tables/terrain-id-to-name.ts";
 
 export default function tmxToTerrain(tmxXml: string) {
-  const rawData = parseTmxXml(tmxXml);
-  const rawLayer = rawData.map.layer;
-  const mapLayers = Array.isArray(rawLayer) ? rawLayer : [rawLayer];
-
-  const tilesetId = "30";
-
-  const layers = mapLayers.map((layer) => {
-    const tiles = layer.data.tile;
-    if (!tiles) return [];
-    const terrainGrid: string[] = [];
-    tiles.forEach((tile) => {
-      const terrainTags = tilesetIdToTerrainId[tilesetId];
-      if (!terrainTags) {
-        throw new Error("Invalid tile config id: " + tilesetId);
-      }
-      const terrainTagsArray = terrainTags.split(" ");
-      const terrainId: number =
-        Number(terrainTagsArray[Number(tile["@_gid"])]) + 1;
-      const terrainName = terrainIdToName[terrainId];
-      terrainGrid.push(terrainName);
+  const { tilesetId, layers, firstGid } = parseTmxXml(tmxXml);
+  const tid = tilesetId;
+  const terrainTags = tilesetIdToTerrainId[tid];
+  if (!terrainTags) {
+    throw new Error("Invalid tile config id: " + tid);
+  }
+  const terrainTagsArray = terrainTags.split(" ");
+  const result = layers.map((layer) => {
+    return layer.gids.map((gid) => {
+      console.log("gid :>> ", gid);
+      const terrainIdIndex = parseInt(terrainTagsArray[gid - firstGid], 10);
+      console.log("terrainIdIndex :>> ", terrainIdIndex);
+      const terrainId = terrainIdIndex;
+      console.log("terrainId :>> ", terrainId);
+      console.log(
+        "terrainIdToName[terrainId] :>> ",
+        terrainIdToName[terrainId]
+      );
+      return terrainIdToName[terrainId] || "--";
     });
-    return terrainGrid;
   });
-  return layers;
+  return result;
 }
 
 if (import.meta.main) {
