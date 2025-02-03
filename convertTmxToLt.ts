0c839d8f754390de52d035702c3aa1d1c2856677
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2025-02-02 18:58:51 -0600

    Update convertTmxToLt.ts

--- convertTmxToLt.ts
+++ convertTmxToLt.ts
@@ -4,12 +4,17 @@ import parseTmxXml from "@/lib/parseTmxXml.ts";
 export default async function convertTmxToLT(tmxFilePath: string) {
   const tmxXml = await Deno.readTextFile(tmxFilePath);
   const tmxData = parseTmxXml(tmxXml);
-  const terrainLayers = tmxToTerrain(tmxData);
-  console.log("terrainLayers :>> ", terrainLayers);
+  const layersWithTerrainName = tmxToTerrain(tmxData);
+  console.log("layersWithTerrainName :>> ", layersWithTerrainName);
+  // logs:
+  // layersWithTerrainName :>>  [
+  //   [
+  //     "Wall",   "Plains", "Wall",   "Wall",   "Wall",   "Forest",
+  //     "Plains", "Hill",   "Hill",   "Hill",   "Plains", "River", ...
 }
 
 if (import.meta.main) {
-  const parsedResult = convertTmxToLT("examples/map/field.tmx");
+  const parsedResult = await convertTmxToLT("examples/map/field.tmx");
   console.log("parsedResult :>> ", parsedResult);
 }
 
