8d5ac8de59a13c2c50fa9bd479d0ffc2318e5cc0
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2025-02-07 17:05:36 -0600

    update map previews

--- process-all-maps.ts
+++ process-all-maps.ts
@@ -1,11 +1,13 @@
 import importMaps from "@/import-maps/import-maps.ts";
 import downloadMaps from "@/import-maps/download-maps.ts";
 import runConversion from "@/convert-all.ts";
+import generateMapPreviews from "@/generate-map-previews.ts";
 
 export default async function processAllMaps(): Promise<void> {
   await importMaps();
   await downloadMaps();
   await runConversion();
+  await generateMapPreviews();
 }
 
 if (import.meta.main) {
